a2081810dd28253c3b50d5b8ef5c43e15f528f29
Jason Brown <jbrown8181@gmail.com>
2024-10-08 12:33:53 +0800

    update styles import

--- seedlot-site/src/app/components/WalletAuth/WalletAuth.tsx
+++ seedlot-site/src/app/components/WalletAuth/WalletAuth.tsx
@@ -1,20 +1,20 @@
-// import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import dynamic from "next/dynamic";
 import Image from "next/image";
 
-// WalletAuth component
+
 export const WalletAuth = () => {
 
-  const WalletMultiButton = dynamic(
-    () =>
-      import("@solana/wallet-adapter-react-ui").then(
-        (mod) => mod.WalletMultiButton
-      ),
-    {
-      loading: () => <Image src="/images/loading_spinner.svg" alt="Loading..." width={24} height={24} />,
-      ssr: false, // Disable server-side rendering to ensure this only loads on the client
-    }
-  );
+  // const WalletMultiButton = dynamic(
+  //   () =>
+  //     import("@solana/wallet-adapter-react-ui").then(
+  //       (mod) => mod.WalletMultiButton
+  //     ),
+  //   {
+  //     loading: () => <Image src="/images/loading_spinner.svg" alt="Loading..." width={24} height={24} />,
+  //     ssr: false, // Disable server-side rendering to ensure this only loads on the client
+  //   }
+  // );
 
   return (
     <div>
--- seedlot-site/src/app/walletProvider.tsx
+++ seedlot-site/src/app/walletProvider.tsx
@@ -5,10 +5,9 @@ import {
   WalletProvider as SolanaWalletProvider,
 } from "@solana/wallet-adapter-react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+// import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-
-// require("@solana/wallet-adapter-react-ui/styles.css
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 interface WalletProviderProps {
   children: ReactNode;
@@ -18,11 +17,11 @@ export const WalletProvider: FC<WalletProviderProps> = ({ children }) => {
   const network = WalletAdapterNetwork.Devnet;
   const endpoint = useMemo(() => "https://api.devnet.solana.com", []);
 
-  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+  // const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
-      <SolanaWalletProvider wallets={wallets} autoConnect>
+      <SolanaWalletProvider wallets={[]} autoConnect>
         <WalletModalProvider>{children}</WalletModalProvider>
       </SolanaWalletProvider>
     </ConnectionProvider>
